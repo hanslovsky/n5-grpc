52c24b2a51a9d120e363460754091010d69c25ef
Philipp Hanslovsky <philipp.hanslovsky@gmail.com>
2021-10-23 23:54:54 -0400

    Tests WIP

--- lib/src/test/kotlin/me/hanslovsky/n5/grpc/N5GrpcReaderTest.kt
+++ lib/src/test/kotlin/me/hanslovsky/n5/grpc/N5GrpcReaderTest.kt
@@ -1,19 +1,24 @@
 package me.hanslovsky.n5.grpc
 
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import com.google.gson.JsonPrimitive
 import me.hanslovsky.n5.grpc.service.N5GrpcServer
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 internal class N5GrpcReaderTest {
 
+
+
     @Test
     fun getDatasetAttributes() {
-        TODO("Do this")
+//        TODO("Do this")
     }
 
     @Test
     fun readBlock() {
-        TODO("Do this")
+//        TODO("Do this")
     }
 
     @Test
@@ -38,11 +43,19 @@ internal class N5GrpcReaderTest {
 
     @Test
     fun list() {
-        TODO("Do this")
+//        TODO("Do this")
     }
 
     @Test
     fun getAttributes() {
-        TODO("Do this")
+        val server = N5GrpcServer(9090, TestService(getAttributes = { N5Grpc.JsonString.newBuilder().setJsonString(if (it.pathName == "my/group") "{\"foo\":\"bar\"}" else "{}").build() }))
+        val reader = N5GrpcReader(host="localhost", port=9090)
+        server.start()
+        println(defaultGson.toJson(mapOf("foo" to "bar")))
+        println("{\"foo\":\"bar\"}")
+        println(defaultGson.toJson(mapOf("foo" to "bar")) == "{\"foo\":\"bar\"}")
+        Assertions.assertEquals(mapOf("foo" to JsonPrimitive("bar")), reader.getAttributes("my/group"))
+        Assertions.assertEquals(mapOf<String, JsonElement>(), reader.getAttributes("my/dataset"))
+        server.stop()
     }
 }
