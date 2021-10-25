195b4f3c9efb0f833e7bc6b9d9212852806f3b2b
Philipp Hanslovsky <philipp.hanslovsky@gmail.com>
2021-10-24 22:24:39 -0400

    Improve test design

--- lib/src/test/kotlin/me/hanslovsky/n5/grpc/N5GrpcReaderTest.kt
+++ lib/src/test/kotlin/me/hanslovsky/n5/grpc/N5GrpcReaderTest.kt
@@ -1,15 +1,19 @@
 package me.hanslovsky.n5.grpc
 
 import com.google.gson.JsonElement
-import com.google.gson.JsonObject
 import com.google.gson.JsonPrimitive
 import me.hanslovsky.n5.grpc.service.N5GrpcServer
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback
+import org.junit.jupiter.api.extension.BeforeTestExecutionCallback
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.ExtensionContext
+
 
 internal class N5GrpcReaderTest {
-
-
 
     @Test
     fun getDatasetAttributes() {
@@ -21,24 +25,20 @@ internal class N5GrpcReaderTest {
 //        TODO("Do this")
     }
 
+    @ExtendWith(EnableExists::class)
     @Test
     fun exists() {
-        val server = N5GrpcServer(9090, TestService(datasetExists = { N5Grpc.BooleanFlag.newBuilder().setFlag((it.pathName == "my/group")).build() }))
         val reader = N5GrpcReader(host="localhost", port=9090)
-        server.start()
         Assertions.assertTrue(reader.datasetExists("my/group"))
         Assertions.assertFalse(reader.datasetExists("my/dataset"))
-        server.stop()
     }
 
+    @ExtendWith(EnableDatasetExists::class)
     @Test
     fun datasetExists() {
-        val server = N5GrpcServer(9090, TestService(datasetExists = { N5Grpc.BooleanFlag.newBuilder().setFlag((it.pathName == "my/dataset")).build() }))
         val reader = N5GrpcReader(host="localhost", port=9090)
-        server.start()
         Assertions.assertFalse(reader.datasetExists("my/group"))
         Assertions.assertTrue(reader.datasetExists("my/dataset"))
-        server.stop()
     }
 
     @Test
@@ -46,16 +46,45 @@ internal class N5GrpcReaderTest {
 //        TODO("Do this")
     }
 
+    @ExtendWith(EnableGetAttributes::class)
     @Test
     fun getAttributes() {
-        val server = N5GrpcServer(9090, TestService(getAttributes = { N5Grpc.JsonString.newBuilder().setJsonString(if (it.pathName == "my/group") "{\"foo\":\"bar\"}" else "{}").build() }))
         val reader = N5GrpcReader(host="localhost", port=9090)
-        server.start()
-        println(defaultGson.toJson(mapOf("foo" to "bar")))
-        println("{\"foo\":\"bar\"}")
-        println(defaultGson.toJson(mapOf("foo" to "bar")) == "{\"foo\":\"bar\"}")
         Assertions.assertEquals(mapOf("foo" to JsonPrimitive("bar")), reader.getAttributes("my/group"))
         Assertions.assertEquals(mapOf<String, JsonElement>(), reader.getAttributes("my/dataset"))
-        server.stop()
+    }
+
+    class EnableGetDatasetAttributes
+
+    class EnableReadBlock
+
+    class EnableExists : AfterTestExecutionCallback, BeforeTestExecutionCallback {
+        override fun beforeTestExecution(context: ExtensionContext?) { testService.datasetExists = { N5Grpc.BooleanFlag.newBuilder().setFlag((it.pathName == "my/group")).build() } }
+        override fun afterTestExecution(context: ExtensionContext?) { testService.datasetExists = null }
+    }
+
+    class EnableDatasetExists : AfterTestExecutionCallback, BeforeTestExecutionCallback {
+        override fun beforeTestExecution(context: ExtensionContext?) { testService.datasetExists = { N5Grpc.BooleanFlag.newBuilder().setFlag((it.pathName == "my/dataset")).build() } }
+        override fun afterTestExecution(context: ExtensionContext?) { testService.datasetExists = null }
+    }
+
+    class EnableList
+
+    class EnableGetAttributes : AfterTestExecutionCallback, BeforeTestExecutionCallback{
+        override fun beforeTestExecution(context: ExtensionContext?) { testService.getAttributes = { N5Grpc.JsonString.newBuilder().setJsonString(if (it.pathName == "my/group") "{\"foo\":\"bar\"}" else "{}").build() } }
+        override fun afterTestExecution(context: ExtensionContext?) { testService.datasetExists = null }
+    }
+
+    companion object {
+        private val testService = TestService()
+        private val server = N5GrpcServer(9090, testService)
+
+        @JvmStatic
+        @BeforeAll
+        fun startServer() = server.start()
+
+        @JvmStatic
+        @AfterAll
+        fun stopServer() = server.stop()
     }
 }
--- lib/src/test/kotlin/me/hanslovsky/n5/grpc/TestService.kt
+++ lib/src/test/kotlin/me/hanslovsky/n5/grpc/TestService.kt
@@ -5,44 +5,56 @@ import N5Grpc
 import io.grpc.stub.StreamObserver
 
 class TestService(
-    private val readBlock: ((N5Grpc.BlockMeta) -> N5Grpc.Block)? = null,
-    private val getAttributes: ((N5Grpc.Path) -> N5Grpc.JsonString)? = null,
-    private val getDatasetAttributes: ((N5Grpc.Path) -> N5Grpc.DatasetAttributes)? = null,
-    private val exists: ((N5Grpc.Path) -> N5Grpc.BooleanFlag)? = null,
-    private val list: ((N5Grpc.Path) -> N5Grpc.Paths)? = null,
-    private val datasetExists: ((N5Grpc.Path) ->N5Grpc.BooleanFlag)? = null
+    var readBlock: ((N5Grpc.BlockMeta) -> N5Grpc.Block)? = null,
+    var getAttributes: ((N5Grpc.Path) -> N5Grpc.JsonString)? = null,
+    var getDatasetAttributes: ((N5Grpc.Path) -> N5Grpc.DatasetAttributes)? = null,
+    var exists: ((N5Grpc.Path) -> N5Grpc.BooleanFlag)? = null,
+    var list: ((N5Grpc.Path) -> N5Grpc.Paths)? = null,
+    var datasetExists: ((N5Grpc.Path) ->N5Grpc.BooleanFlag)? = null
 ) : N5GRPCServiceGrpc.N5GRPCServiceImplBase() {
     override fun readBlock(request: N5Grpc.BlockMeta, responseObserver: StreamObserver<N5Grpc.Block>) {
-        if (readBlock == null) TODO("Implementation not provided")
-        responseObserver.onNextAndOnCompleted(readBlock.invoke(request))
+        readBlock.let { readBlock ->
+            if (readBlock == null) TODO("Implementation not provided")
+            responseObserver.onNextAndOnCompleted(readBlock.invoke(request))
+        }
     }
 
     override fun getAttributes(request: N5Grpc.Path, responseObserver: StreamObserver<N5Grpc.JsonString>) {
-        if (getAttributes == null) TODO("Implementation not provided")
-        responseObserver.onNextAndOnCompleted(getAttributes.invoke(request))
+        getAttributes.let { getAttributes ->
+            if (getAttributes == null) TODO("Implementation not provided")
+            responseObserver.onNextAndOnCompleted(getAttributes.invoke(request))
+        }
     }
 
     override fun getDatasetAttributes(
         request: N5Grpc.Path,
         responseObserver: StreamObserver<N5Grpc.DatasetAttributes>
     ) {
-        if (getDatasetAttributes == null) TODO("Implementation not provided")
-        responseObserver.onNextAndOnCompleted(getDatasetAttributes.invoke(request))
+        getDatasetAttributes.let { getDatasetAttributes ->
+            if (getDatasetAttributes == null) TODO("Implementation not provided")
+            responseObserver.onNextAndOnCompleted(getDatasetAttributes.invoke(request))
+        }
     }
 
     override fun exists(request: N5Grpc.Path, responseObserver: StreamObserver<N5Grpc.BooleanFlag>) {
-        if (exists == null) TODO("Implementation not provided")
-        responseObserver.onNextAndOnCompleted(exists.invoke(request))
+        exists.let { exists ->
+            if (exists == null) TODO("Implementation not provided")
+            responseObserver.onNextAndOnCompleted(exists.invoke(request))
+        }
     }
 
     override fun list(request: N5Grpc.Path, responseObserver: StreamObserver<N5Grpc.Paths>) {
-        if (list == null) TODO("Implementation not provided")
-        responseObserver.onNextAndOnCompleted(list.invoke(request))
+        list.let { list ->
+            if (list == null) TODO("Implementation not provided")
+            responseObserver.onNextAndOnCompleted(list.invoke(request))
+        }
     }
 
     override fun datasetExists(request: N5Grpc.Path, responseObserver: StreamObserver<N5Grpc.BooleanFlag>) {
-        if (datasetExists == null) TODO("Implementation not provided")
-        responseObserver.onNextAndOnCompleted(datasetExists.invoke(request))
+        datasetExists.let { datasetExists ->
+            if (datasetExists == null) TODO("Implementation not provided")
+            responseObserver.onNextAndOnCompleted(datasetExists.invoke(request))
+        }
     }
 }
 
